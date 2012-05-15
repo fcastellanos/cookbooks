e319167f6882ae6ff00fe5e9de7580b14e56ab31
Mario Zaizar <mariozaizar@gmail.com>
2012-05-15 13:14:21 -0500

    System recipe.

---
+++ cookbooks/system/recipes/default.rb
@@ -0,0 +1,8 @@
+# Update apt-get and locate
+# execute "apt-get update"
+# execute "updatedb" # locale
+
+# Utilities
+package "build-essential" # make, gcc, headers
+package "git-core" # 1.7.9.5
+package "vim" # 7.3
